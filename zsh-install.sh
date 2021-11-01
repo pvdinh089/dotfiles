05a993778bba1c205adaa4e1b073c9d49e9f18c1
pvdinh089 <pvdinh089@gmail.com>
2021-11-01 19:20:30 +0700

    update

---
+++ zsh-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Cài đặt zsh
+sudo pacman -S --noconfirm zsh
+
+# Cài đặt oh-my-zsh
+$ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Cài đặt zsh - autosuggestion
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+cp -r .zshrc ~
+
+# Cài đặt paru
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd $HOME
