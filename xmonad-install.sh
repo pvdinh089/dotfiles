6088d1873a36516b076d090433763246da69bbbd
Dinh Pham Van <64542408+pvdinh089@users.noreply.github.com>
2021-11-03 10:15:28 +0700

    Update xmonad-install.sh

--- xmonad-install.sh
+++ xmonad-install.sh
@@ -5,9 +5,6 @@ sudo pacman -S --noconfirm xorg ligthdm lightdm-gtk-greeter xmonad xmonad-contri
 # Kích hoạt dịch vụ quản lý Desktop
 sudo systemctl enable lightdm
 
-# Tải thư viện đã config sẵn trên github
-git clone https://github.com/pvdinh089/dotfiles
-
 sleep 5
 cd dotfiles
 cp -r .config $HOME
